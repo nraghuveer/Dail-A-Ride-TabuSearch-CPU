d469699af6db981cd164c86fb46565df45432e1e
Raghuveer N <narahar2@uwm.edu>
2022-11-13 18:23:29 -0600

    init version
    request parsing
    Average departure time calculation

---
+++ src/gts.py
@@ -0,0 +1,138 @@
+from typing import List, Dict, Tuple
+from itertools import product
+from parse_requests import Request, getRequests
+
+Point = Tuple[int, int]
+
+"""
+earliest departure -> whats the earliest time that the customer is avaiable for pick or we can pikup customer
+    we can after this time (pickup)
+latest departure -> whats the atmost time we cna dropoff
+    we can do before this time (dropoff)
+"""
+
+"""
+Granular tabu search implemenation of dail-a-ride algorithm
+GTS is a TS with a particular focus on the local search phase
+The size of GTS is regulated by a granular threshold
+
+It is based on the idea that the solutions rarely contain long edges
+the algorithm explores only edges with a length up to a certain threshold
+during the local seaerch phase.
+
+Simple Moves:
+1. Move a request from a route into another one
+2. insert an unserved request in a route, or remove a request from route
+A granular neighborhood, the only moves allowed are those with reduced cost
+c_ij < T_Gran
+"""
+
+
+class GTS:
+    def __init__(self) -> None:
+        self.requests: List[Request] = getRequests()
+        # build coordinates
+        # each request has both pickup and dropoff coordinates
+        # use +req.id as pickup as -req.id as dropoff
+        self.coords: Dict[int, Point] = {}
+        for req in self.requests:
+            self.coords[req.id] = req.src_point()
+            self.coords[-req.id] = req.dst_point()
+
+        # service time at each node -> in minutes
+        # this is something we came up with
+        self.d: Dict[str, int] = {}
+        for req in self.requests:
+            self.d[req.id] = 2
+            self.d[-req.id] = 2
+
+        """
+        [e, l] represents a time window
+        DAR system should pickup/dropoff customer
+        at in this time window at respective node
+        """
+        tw = {}
+        # add 5 min time window
+        offset = int(5 * 60)
+        for req in self.requests:
+            id = req.id
+            tw[id] = (req.pickup_time, req.pickup_time + offset)
+            tw[-id] = (req.dropoff_time, req.dropoff_time + offset)
+        self.tw = tw
+
+    def start(self):
+        # calculate _D_ij for all node combinations
+        results = []
+        for pointI, pointJ in product(self.requests, self.requests[1:]):
+            if pointI.id == pointJ.id:
+                continue
+            res = self.adt(pointI.id, pointJ.id)
+            results.append((f"{pointI.points()}", f"{pointJ.points()}", res))
+
+        results = sorted(results, key=lambda x: x[2])
+        print("Average departure time, how close they are in spatial and temporal")
+        print("\n".join([f"(from={x[0]}, to={x[1]}, d={x[2]})" for x in results]))
+
+    def travel_time(self, one: int, two: int):
+        pone = self.coords[one]
+        ptwo = self.coords[two]
+        return abs(pone[0]-ptwo[0]) + abs(pone[1]-ptwo[1])
+
+    def adt(self, i: int, j: int):
+        """ returns D~_ij """
+        """ This is used measure spatial and temoral distance between two requests i and j"""
+
+        # Following are possible sequences
+        # i+, i-, j+, j- => pi1
+        # i+, j+, i-, j- => pi2
+        # i+, j+, j-, i- => pi3
+        def for_sequence(one, two, three, four):
+            start_tw = self.tw[one][0]
+            return start_tw + self.d[one] + \
+                self.travel_time(one, two) + self.d[two] + \
+                self.travel_time(two, three) + self.d[three] + \
+                self.travel_time(three, four) + self.d[four]
+
+        # since j+ is arrival node here, if we arrive early than the actual arrival
+        # we have to wait till the arrival
+        # TODO: fix this
+        wait_time_at_j = 0
+        D_pi1 = for_sequence(i, -i, j, -j) + wait_time_at_j
+        D_pi2 = for_sequence(i, j, -i, -j) + wait_time_at_j
+        D_pi3 = for_sequence(i, j, -j, -i) + wait_time_at_j
+
+        # TODO: check if load or time constraints are respected at each node
+        # for now, assume all sequences are feasible
+        return D_pi1 + D_pi2 + D_pi3
+
+    def get_service_quality(self, i_arr, i_dep):
+        # end of time window at destination
+        a = self.time_windows[i_dep]
+
+        # start of service time at arrival
+        e, _ = self.service_time[i_arr]
+        t = self.trave_time[(i_arr, i_dep)]
+        return (a - e) / t
+
+    # def objective_function(self):
+    #     cust = []
+    #     N = self.arrivals[:]
+    #     N.extend(self.departures[:])
+    #     for i in self.arrivals:
+    #        for v in self.vehicles:
+    #            for j in N:
+    #                 val = self.get_x(i, j, v) - self.get_service_quality(i, j)
+    #                 cust.append(self.alpha_coefficient * val)
+    #     return sum(cust)
+
+if __name__ == "__main__":
+    gts = GTS()
+    gts.start()
+
+
+
+
+
+
+
+
---
+++ src/parse_requests.py
@@ -0,0 +1,101 @@
+# Growth dataset -> fixed service duration and area of service while the number of requests varies from 50 to 1000
+from typing import Union
+"""
+SrcPoint_x= The x-coordinate of customer pick-up point.
+
+SrcPoint_y= The y-coordinate of customer pick-up point.
+
+DstPoint_x= The x-coordinate of customer drop-off point.
+
+DstPoint_y= The y-coordinate of customer drop-off point.
+
+DRT= Direct Ride Time. The time needed to transport the customer between Src and Dst point on the shortest path without any deviations.
+
+MRT= Maximum Ride Time (calculated by adding 60 minutes to the direct ride time of the customer).
+
+Pick-up/Drop-up time= Pickup or drop-off time of the customer determined by the flag.
+
+Flag= A Boolean value that indicates if the time in previous column is pick-up (1) or drop-off (0) time.
+"""
+
+ALPHA = 0.8  # Alpha coefficient, used to calculate the dropoff time window
+IDX = 0
+SRC_POINT_X = 1
+SRC_POINT_Y = 2
+DST_POINT_X = 3
+DST_POINT_Y = 4
+DRT = 5
+MRT= 6
+PICKUP_OR_DROPOFF_TIME = 7
+ISPICK_TIME = 8
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Request:
+    def __init__(self,id, line: str, service_duration: int, area_of_service: int):
+
+        self.service_duration = service_duration
+        self.area_of_service = area_of_service
+
+        parts = filter(lambda x: x!="", line.split("\t"))
+        for i, x in enumerate(parts):
+            x = x.strip("\t")
+            val = int(x)
+            if i == IDX:
+                self.id = id
+            elif i == SRC_POINT_X:
+                self.src_x = val
+            elif i == SRC_POINT_Y:
+                self.src_y = val
+            elif i == DST_POINT_X:
+                self.dst_x = val
+            elif i == DST_POINT_Y:
+                self.dst_y = val
+            elif i == DRT:
+                self.drt = val
+            elif i == MRT:
+                self.mrt = val
+            elif i == PICKUP_OR_DROPOFF_TIME:
+                self.__time = val
+            elif i == ISPICK_TIME:
+                if val == 1:
+                    self.pickup_time = self.__time
+                    # dropoff = alpha * maxride + pickup
+                    self.dropoff_time = int(ALPHA * self.mrt) + self.pickup_time
+
+    def src_point(self):
+        return (self.src_x, self.src_y)
+
+    def dst_point(self):
+        return (self.dst_x, self.dst_y)
+
+    def points(self):
+        return [self.src_point(), self.dst_point()]
+
+    def __str__(self):
+        src = f"SRC=({self.src_x}, {self.src_y})"
+        dst = f"DST=({self.dst_x}, {self.dst_y})"
+        drt = f"DRT={self.drt}"
+        mrt = f"MRT={self.mrt}"
+        picktime = f"PICKTIME={self.pickup_time}"
+        sd = f"SERVICE_DURATION={self.service_duration} min"
+        aos = f"AREA_OF_SERVICE={self.area_of_service} miles"
+
+        return " | ".join([src, dst, drt, mrt, picktime, sd, aos])
+
+def sqKm_to_sqMiles(km: float) -> float:
+    return km * 0.386102
+
+def getRequests():
+    filepath = "DARPDATASET/Temportal-DS/nCustomers_50/Temporal_SD2hrs_SA10km.txt"
+    service_time = 120 # minutes
+    area_of_service = sqKm_to_sqMiles(10) # square km
+
+    requests = []
+    with open(filepath, 'r') as f:
+        for id, line in enumerate(f.readlines()):
+            requests.append(Request(id, line, service_time, area_of_service))
+    return requests
